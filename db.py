17cba0fc0c43e0f2edbe4c4c6037599a5263e40d
brunovicenters <brunovicente.dev@gmail.com>
2025-05-08 17:14:55 -0300

    Changed to db_connection

--- db.py
+++ db.py
@@ -5,27 +5,14 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DB_USER     = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST     = os.getenv("DB_HOST")
-DB_PORT     = os.getenv("DB_PORT")
-DB_NAME     = os.getenv("DB_NAME")
+DB_CONNECTION     = os.getenv("DB_CONNECTION")
 
 missing = [k for k,v in {
-    "DB_USER":DB_USER,
-    "DB_PASSWORD":DB_PASSWORD,
-    "DB_HOST":DB_HOST,
-    "DB_PORT":DB_PORT,
-    "DB_NAME":DB_NAME
+    "DB_CONNECTION":DB_CONNECTION,
 }.items() if not v]
 if missing:
     raise RuntimeError(f"Faltam estas env vars em .env: {missing}")
 
-DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
-
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DB_CONNECTION, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 
